6a84b558d99a0a1b25dc192187db87353054e5ac
USER <user@Ikennas-MacBook-Pro.local>
2023-02-06 14:56:58 +0100

    first commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': 'f66bc88ee2mshd95068301203fdep1a6140jsn5d94cd329dfe',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
+
